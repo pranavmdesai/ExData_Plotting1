f85c81bcf8d23523abbcb69fda5d7df0d42a35ff
pranavmdesai <pranavmdesai@gmail.com>
2015-05-06 22:24:32 -0700

    plot1.R

---
+++ plot1.R
@@ -0,0 +1,18 @@
+#Let's read the data in
+power <- read.csv("household_power_consumption.txt",sep=";",header = T)
+
+#Let's convert the date into character which we can parse
+power$Date <- as.character(power$Date)
+
+#Let's subset the data
+febdata <- subset(power, power$Date=="1/2/2007" | power$Date=="2/2/2007")
+
+febdata$Global_active_power <- as.numeric(febdata$Global_active_power)
+
+#Let's create our histogram with the appropriate x-axis label and color for the bars
+hist(febdata$Global_active_power/500,main = "Global Active Power", col = "Red", 
+     xlab = "Global Active Power (kilowatts), ")
+
+dev.copy(png,"plot1.png")
+dev.off()
+
